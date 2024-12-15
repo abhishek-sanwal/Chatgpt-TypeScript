4933c8727c0e7115175e841075712b7cb2b7b5b4
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-12-15 23:12:39 +0530

    fix(ci): update script to use npm commands

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from 'react'
 
 function App(): ReactElement {
-
   return (
     <div className="p-20 border shadow-md border-gray-50 rounded-xl bg-black text-white">
       Hello World
