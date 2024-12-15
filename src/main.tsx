ac015806c067f6f8d996e62454bfcd301c5ef46f
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-12-15 22:56:25 +0530

    refactor: updated template to include conventional typescript-config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
 import './App.css'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import App from './App'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
@@ -9,21 +11,6 @@ createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route
-          path="/about"
-          element={
-            <>
-              <div className="text-center">
-                <h1 className="text-xl">About</h1>
-                <div>
-                  <Link to="/" className="text-purple-400 underline">
-                    Home
-                  </Link>
-                </div>
-              </div>
-            </>
-          }
-        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
